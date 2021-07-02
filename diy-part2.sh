d568cee048be88f05b8a92a0f2229e62b9ffc2f7
jasonandy999 <59506376+jasonandy999@users.noreply.github.com>
2021-07-02 18:48:33 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -29,10 +29,10 @@ sed -i '/uci commit system/i\uci set system.@system[0].hostname='K2P'' package/d
 sed -i 's/ssid=OpenWrt/ssid=iMei/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
 # 修改默认wifi密码key为12345678
-#sed -i 's/key=password/key=12345678/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i 's/key=password/key=12345678/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
 # 设置密码为空（安装固件时无需密码登陆，然后自己修改想要的密码）
-sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
+#sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
 
 
 git clone https://github.com/jasonandy999/packages.git package/lean/luci-app-frpc
